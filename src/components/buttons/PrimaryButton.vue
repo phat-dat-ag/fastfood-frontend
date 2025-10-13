fe95bcf4d208c2c573b586538d642eb10efdd6dc
phat-dat-ag <67agphatdat@gmail.com>
2025-10-13 09:25:30 +0700

    feat(buttons): add onClick prop to PrimaryButton component

--- src/components/buttons/PrimaryButton.vue
+++ src/components/buttons/PrimaryButton.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 const props = defineProps<{
     label: string;
+    onClick?: () => void;
 }>();
 </script>
 <template>
     <button class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200 font-semibold"
-        type="submit">
+        type="submit" @click="props.onClick?.()">
         {{ props.label }}
     </button>
 </template>
