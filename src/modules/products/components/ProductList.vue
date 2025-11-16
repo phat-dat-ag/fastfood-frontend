0d71de07d22f4dc71d9d7cf192687ba0dd950717
phat-dat-ag <67agphatdat@gmail.com>
2025-11-16 19:24:52 +0700

    style(products): responsive grid layout for mobile and tablet in ProductList

--- src/modules/products/components/ProductList.vue
+++ src/modules/products/components/ProductList.vue
@@ -13,7 +13,7 @@ function handleAdd(product: Product) {
 }
 </script>
 <template>
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <div v-for="product in props.products" :key="product.id">
             <ProductCard :product="product" @add-to-cart="handleAdd" />
         </div>
