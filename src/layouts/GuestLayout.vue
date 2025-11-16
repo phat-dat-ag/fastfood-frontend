7c20cb1ae33f5f4b638d17ec71db8dbd2522ed04
phat-dat-ag <67agphatdat@gmail.com>
2025-11-16 18:48:10 +0700

    fix(layouts): unify main content width for mobile, tablet, and desktop

--- src/layouts/GuestLayout.vue
+++ src/layouts/GuestLayout.vue
@@ -5,9 +5,9 @@ import { USER_ROLES } from '../constants/user-roles';
 </script>
 
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col">
     <Header :role="USER_ROLES.GUEST"></Header>
-    <main class="w-[80%] my-4 mx-auto">
+    <main class="w-full px-3 sm:w-[90%] lg:w-[80%] my-4 mx-auto">
       <router-view></router-view>
     </main>
     <Footer></Footer>
--- src/layouts/StaffLayout.vue
+++ src/layouts/StaffLayout.vue
@@ -4,9 +4,11 @@ import Footer from '../components/Footer.vue';
 import { USER_ROLES } from '../constants/user-roles';
 </script>
 <template>
-    <Header :role="USER_ROLES.STAFF"></Header>
-    <main class="w-[80%] my-4 mx-auto">
-        <router-view></router-view>
-    </main>
-    <Footer></Footer>
+    <div class="min-h-screen flex flex-col">
+        <Header :role="USER_ROLES.STAFF"></Header>
+        <main class="w-full px-3 sm:w-[90%] lg:w-[80%] my-4 mx-auto">
+            <router-view></router-view>
+        </main>
+        <Footer></Footer>
+    </div>
 </template>
