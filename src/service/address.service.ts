4562dbb3ba739470d8e0bfd25951412668c24bc3
phat-dat-ag <67agphatdat@gmail.com>
2026-03-22 11:47:09 +0700

    refactor(address): update API calls to match RESTful address endpoints

--- src/service/address.service.ts
+++ src/service/address.service.ts
@@ -2,13 +2,13 @@ import api from "../api/axios";
 import type { AddressCreateRequest } from "../types/geocode.types";
 
 export const createAddress = (data: AddressCreateRequest) => {
-  return api.post("/address", data);
+  return api.post("/addresses", data);
 };
 
 export const getAddresses = () => {
-  return api.get("/address");
+  return api.get("/addresses");
 };
 
-export const deleteAddress = (id: number) => {
-  return api.delete("/address", { params: { id } });
+export const deleteAddress = (addressId: number) => {
+  return api.delete(`/addresses/${addressId}`);
 };
